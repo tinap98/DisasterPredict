d232064724e2eae00a168c6fcd40e5299ae3685c
tinap98 <22b81a05bs@cvr.ac.in>
2025-04-11 20:46:08 +0530

    plswork

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+//dope
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
